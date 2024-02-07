57c05f7fbc3115e9151253d3bf94292a4ec8ad11
Voitenko Illia <79865476+IlyaVoitenko@users.noreply.github.com>
2024-02-07 20:00:38 +0100

    added special  function for client_secret and payment

--- server.js
+++ server.js
@@ -4,21 +4,30 @@ require("dotenv").config();
 const stripe = require("stripe")(process.env.SECRET_KEY);
 const bodyParser = require("body-parser");
 const cors = require("cors");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(cors());
 
-app.post("/payment", cors(), async (req, res) => {
-  let { amount, id } = req.body;
+app.post("/payment-intents", async (req, res) => {
   try {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount,
-      currency: "USD",
-      description: "Spatula company",
-      payment_method: id,
+    const { client_secret } = await stripe.paymentIntents.create({
+      amount: 1099,
+      currency: "usd",
+      payment_method_types: ["card"],
+      metadata: {
+        order_id: "6735",
+      },
     });
+    res.send({ client_secret });
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
+});
 
+app.post("/payment", cors(), async (req, res) => {
+  try {
     const confirmedPaymentIntent = await stripe.paymentIntents.confirm(
       paymentIntent.id,
       { return_url: process.env.RETURN_URL }
@@ -30,7 +39,6 @@ app.post("/payment", cors(), async (req, res) => {
       success: true,
     });
   } catch (error) {
-    console.log("Error", error);
     res.status(500).json({
       message: "Payment failed",
       success: false,
